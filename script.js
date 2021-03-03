d150b33a0d358979966d5f8a19c503beea9abee2
benogit <benporter95@protonmail.com>
2021-03-03 19:04:34 +1000

    Added html layout and some information

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("h2").style.color = "#ff0000";
+    document.getElementById("myP").style.color = "magenta";
+    document.getElementById("myP2").style.color = "blue";
+    document.getElementById("myDiv").style.color = "lightblue";
+}
